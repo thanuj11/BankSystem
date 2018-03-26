9cc7325a10dd56329b7b3e493a897e931cf09c90
thanuj11 <dthanuj28@gmail.com>
2018-03-26 14:45:47 -0500

    first commit

---
+++ BankSystemWeb_Student/src/java/CreateNewAccount.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import java.sql.*;
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author vkris
+ */
+@Named(value = "createNewAccount")
+@RequestScoped
+public class CreateNewAccount {
+
+    /**
+     * Creates a new instance of CreateNewAccount
+     */
+    private String ssn;
+    private double balance;
+    public String createAccount()
+    {
+        //load the driver
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch(Exception e)
+        {
+            return("Internal error,please try again later!");
+        }
+        final String DB_URL = "jdbc:mysql://mis-sql.uhcl.edu/gattupalliv3940";
+        Connection conn = null;
+        Statement stat = null;
+        ResultSet rs = null;
+        String accountID = "";
+        try
+        {
+            conn = DriverManager.getConnection(DB_URL,"gattupalliv3940","1552688");
+            stat = conn.createStatement();
+            rs = stat.executeQuery("select * from nextAccountNumber");
+            if(rs.next())
+            {
+                accountID = ""+rs.getInt(1);
+            }
+            else
+            {
+                return("Internal error,please try again later!");
+            }
+             int j = stat.executeUpdate("Update nextAccountNumber set nextID ='"+(rs.getInt(1)+1)+"'");
+            //insert a new record into bank account table
+            DecimalFormat df = new DecimalFormat("##.00");
+            String s = DateAndTime.DateTime()+":Account opened with an "+"inital balance $"+df.format(balance)+"\n";
+            int r = stat.executeUpdate("insert into BankAccount values ('"+accountID+"','"+ssn+"','"+balance+"','"+s+"')");
+            
+            //update new one 
+           
+            
+            return("Congratualtions you have created a new bank account. The new account number is "+accountID+".");
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+            return("Internal Error!,Please try again later!");
+        }
+        finally
+        {
+            try
+            {
+                rs.close();
+                stat.close();
+                conn.close();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    public String getSsn() {
+        return ssn;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    
+    public CreateNewAccount() {
+    }
+    
+}
